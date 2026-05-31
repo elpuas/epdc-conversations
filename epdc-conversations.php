2c2952523c673434892c72e88c55ad6bacb5ea4c
Alfredo Navas <elpuas@gmail.com>
2026-05-31 13:21:35 -0600

    author updtes

--- epdc-conversations.php
+++ epdc-conversations.php
@@ -5,7 +5,8 @@
  * Version: 0.1.0
  * Requires at least: 6.6
  * Requires PHP: 8.3
- * Author: EPDC
+ * Author: ElPuas Digital Crafts
+ * Author URI: https://elpuasdigitalcrafts.com
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: epdc-conversations
